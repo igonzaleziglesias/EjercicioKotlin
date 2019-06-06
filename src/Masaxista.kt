f25ad3627ac7719c61eda79906bd957a3d2a024e
igonzaleziglesias <43774705+igonzaleziglesias@users.noreply.github.com>
2019-06-06 16:45:25 +0200

    comentado

--- src/Masaxista.kt
+++ src/Masaxista.kt
@@ -2,16 +2,16 @@
 import javax.swing.JOptionPane
 
 class Masaxista(nome: String, apelidos: String, id: Int, edad: Int, var anosExperiencia: Int, var titulacion: String?) :
-    Plantilla(nome, apelidos, id, edad) {
+    Plantilla(nome, apelidos, id, edad) {//hereda de plantilla, los parametros que hereda de plantilla no llevan var los demas si
 
     override fun viaxar() {
-        JOptionPane.showMessageDialog(null, "Viaxa o masaxista:" + super.nome!!)
+        JOptionPane.showMessageDialog(null, "Viaxa o masaxista:" + super.nome!!)//sobreescribir
     }
 
     fun darMasaxes() {}
 
     override fun toString(): String {
-        return "O masaxistata é:\n" + super.toString() + "\nAnosExperiencia=" + anosExperiencia + "\nTitulacion=" + titulacion
+        return "O masaxistata é:\n" + super.toString() + "\nAnosExperiencia=" + anosExperiencia + "\nTitulacion=" + titulacion//sobreescribir
     }
 
 
