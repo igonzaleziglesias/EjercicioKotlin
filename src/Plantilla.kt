715586d46766359759418b59d7e30052bb3931c1
igonzaleziglesias <43774705+igonzaleziglesias@users.noreply.github.com>
2019-06-06 16:42:34 +0200

    comentado

--- src/Plantilla.kt
+++ src/Plantilla.kt
@@ -1,18 +1,18 @@
 
 import javax.swing.JOptionPane
 
-open class Plantilla(var nome: String?, var apelidos: String?, var id: Int, var edad: Int) {
-
+open class Plantilla(var nome: String?, var apelidos: String?, var id: Int, var edad: Int) {//clase plantilla con sus parametros
+//hay q usar open para poder heredar de ella despues
     fun concentrarse() {
-        JOptionPane.showMessageDialog(null, "Concentrase a seleccion.")
+        JOptionPane.showMessageDialog(null, "Concentrase a seleccion.")//metodo concentrase muestra por pantalla un mensaje
     }
 
     open fun viaxar() {
-        JOptionPane.showMessageDialog(null, "Viaxa a seleccion")
+        JOptionPane.showMessageDialog(null, "Viaxa a seleccion")//metodo viaxa muestra por pantalla un mensaje
     }
 
     override fun toString(): String {
-        return "Nome=$nome\nApelidos=$apelidos\nId=$id\nEdad=$edad"
+        return "Nome=$nome\nApelidos=$apelidos\nId=$id\nEdad=$edad"//sobreescribir metodo tostring
     }
 
 }
