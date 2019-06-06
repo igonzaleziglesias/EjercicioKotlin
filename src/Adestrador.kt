0028170237d199cb0d05a1bd04b54cead2e8e028
igonzaleziglesias <43774705+igonzaleziglesias@users.noreply.github.com>
2019-06-06 16:46:36 +0200

    comentado

--- src/Adestrador.kt
+++ src/Adestrador.kt
@@ -2,16 +2,16 @@
 import javax.swing.JOptionPane
 
 class Adestrador(nome: String, apelidos: String, id: Int, edad: Int, var idFederacion: String?) :
-    Plantilla(nome, apelidos, id, edad) {
+    Plantilla(nome, apelidos, id, edad) {//hereda de plantilla, los parametros que hereda no llevan var, los demas si
 
     private fun dirixirPartido() {}
     private fun dirixirAdestramento() {}
     override fun viaxar() {
         JOptionPane.showMessageDialog(null, "Viaxa o entrenador:" + super.nome)
-    }
+    }//sobreescribir
 
     override fun toString(): String {
-        return "O adestrador é:\n" + super.toString() + "\nIdFederacion=" + idFederacion
+        return "O adestrador é:\n" + super.toString() + "\nIdFederacion=" + idFederacion//sobreescribir
     }
 
 }
