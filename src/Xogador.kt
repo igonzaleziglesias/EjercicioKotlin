d72b3af074e6ff6b70b4a067468b04f89e216b16
igonzaleziglesias <43774705+igonzaleziglesias@users.noreply.github.com>
2019-06-06 16:44:20 +0200

    comentado

--- src/Xogador.kt
+++ src/Xogador.kt
@@ -2,16 +2,16 @@
 import javax.swing.JOptionPane
 
 class Xogador(nome: String, apelidos: String, id: Int, edad: Int, var dorsal: Int, var demarcacion: String?) :
-    Plantilla(nome, apelidos, id, edad) {
+    Plantilla(nome, apelidos, id, edad) {//clase xogador hereda de plantilla, los parametros de plantilla no llevan var en el constructor los dema si
 
     private fun xogarPartido() {}
     private fun entrenar() {}
     override fun viaxar() {
-        JOptionPane.showMessageDialog(null, "Viaxan os xogadores:" + super.nome!!)
+        JOptionPane.showMessageDialog(null, "Viaxan os xogadores:" + super.nome!!)//metodo viaxar sobreeescrito
     }
 
     override fun toString(): String {
-        return "O xogador é:\n" + super.toString() + "\nDorsal=" + dorsal + "\nDemarcacion=" + demarcacion
+        return "O xogador é:\n" + super.toString() + "\nDorsal=" + dorsal + "\nDemarcacion=" + demarcacion//metodo tostring sobreescrito
     }
 
 }
